90e92d222120e5c4f927c25a4565e2eef32d338c
songRun518 <163660252+songRun518@users.noreply.github.com>
2026-08-22 19:48:20 +0800

    Rename logf to logfile for logging functions

--- logfn.h
+++ logfn.h
@@ -6,33 +6,33 @@ uint8_t is_log_color_support = 0;
 
 void initLogFn() { is_log_color_support = isatty(STDERR_FILENO); }
 
-#define logf stderr
+#define logfile stderr
 
 #define logDebug(fmt, ...)                                                     \
     do {                                                                       \
         const char* const restrict prefix =                                    \
             is_log_color_support ? "\x1b[1;35m[debug]\x1b[0m" : "[debug]";     \
-        fprintf(logf, "%s " fmt "\n", prefix, ##__VA_ARGS__);                  \
+        fprintf(logfile, "%s " fmt "\n", prefix, ##__VA_ARGS__);               \
     } while (0)
 
 #define logInfo(fmt, ...)                                                      \
     do {                                                                       \
         const char* const restrict prefix =                                    \
             is_log_color_support ? "\x1b[1;32m[info]\x1b[0m" : "[info]";       \
-        fprintf(logf, "%s " fmt "\n", prefix, ##__VA_ARGS__);                  \
+        fprintf(logfile, "%s " fmt "\n", prefix, ##__VA_ARGS__);               \
     } while (0)
 
 #define logWarning(fmt, ...)                                                   \
     do {                                                                       \
         const char* const restrict prefix =                                    \
             is_log_color_support ? "\x1b[1;33m[warning]\x1b[0m" : "[warning]"; \
-        fprintf(logf, "%s " fmt "\n", prefix, ##__VA_ARGS__);                  \
+        fprintf(logfile, "%s " fmt "\n", prefix, ##__VA_ARGS__);               \
     } while (0)
 
 #define logError(fmt, ...)                                                     \
     do {                                                                       \
         const char* const restrict prefix =                                    \
             is_log_color_support ? "\x1b[1;31m[error]\x1b[0m" : "[error]";     \
-        fprintf(logf, "%s " fmt "\n", prefix, ##__VA_ARGS__);                  \
+        fprintf(logfile, "%s " fmt "\n", prefix, ##__VA_ARGS__);               \
     } while (0)
 //
